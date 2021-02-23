9acd7c553b548645110d0a852c056c34520a5c45
tohshine <tohshine@pop-os.localdomain>
2021-02-23 17:37:45 +0100

    final

--- src/config/prod.js
+++ src/config/prod.js
@@ -2,5 +2,7 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     JWTKEY: process.env.JWT_KEY,
-    sendGridKEY: process.env.SENDGRID_KEY
+    sendGridKEY: process.env.SENDGRID_KEY,
+    accessKeyId: process.env.ACCESS_ID,
+    secretAccessKey: process.env.SECRET_KEY
 };
