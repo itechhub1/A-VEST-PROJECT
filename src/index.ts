8f47475e4fcc712bf778caef3ac8c78234349d90
tohshine <tohshine@pop-os.localdomain>
2021-02-23 12:26:12 +0100

    testing

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
   //server asset
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) =>
+  app.get("*", (req, res) => 
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
